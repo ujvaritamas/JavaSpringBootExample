96e8567e7f9f9e4e5a19a2bb7ad98a0fd40230b4
ujvaritamas <ujvaritamas13@gmail.com>
2024-01-13 17:00:59 +0100

    postgres spring connection

---
+++ postgresql_database_example/database/src/main/java/com/myexample/database/DatabaseApplication.java
@@ -0,0 +1,35 @@
+package com.myexample.database;
+
+import javax.sql.DataSource;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+@SpringBootApplication
+public class DatabaseApplication implements CommandLineRunner{
+	
+	private final DataSource dataSource;
+
+	private static final Logger log = LoggerFactory.getLogger(DatabaseApplication.class);
+	
+	public DatabaseApplication(final DataSource dataSource) {
+		super();
+		this.dataSource = dataSource;
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatabaseApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		log.info("Test -   DataSource: "+dataSource.toString());
+		final JdbcTemplate restTemplate = new JdbcTemplate(dataSource);
+		restTemplate.execute("select 1");
+	}
+
+}
---
+++ postgresql_database_example/database/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO widgets(id, name, purpose) VALUES
+(1, 'Widget A', 'Used for testing A'),
+(2, 'Widget B', 'Used for testing B'),
+(3, 'Widget C', 'Used for testing C'),
+(4, 'Widget D', 'Used for testing D'),
+(5, 'Widget E', 'Used for testing E');
